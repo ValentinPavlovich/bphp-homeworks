a05aceefd777d991b52ab127a665062d58a77bf4
ValentinPavlovich <valentin_72@mail.ru>
2019-08-15 08:57:20 +0300

    Task 1.3, 1

---
+++ 1.3-arrays-and-functions/1-places/index.php
@@ -0,0 +1,44 @@
+<?php
+
+    function generate($rows, $placesPerRow, $chairs){
+        if ($rows * $placesPerRow > $chairs) {
+            return false;
+        }
+        $arr = [];
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $placesPerRow; $j++) {
+                $arr[$i][$j] = false;
+            }
+        }
+        return $arr;
+    }
+
+    function reserve($map, $row, $place) {
+        if ($map[$row - 1][$place - 1] === false) {
+            global $map; 
+            $map[$row - 1][$place - 1] = true;
+            return true;
+        }
+        return false;
+    }    
+
+$chairs = 50;    
+$map = generate(5, 8, $chairs);
+$requiredRow = 3;
+$requiredPlace = 5;
+    
+$reverve = reserve($map, $requiredRow, $requiredPlace);
+logReserve($requiredRow, $requiredPlace, $reverve);
+
+$reverve = reserve($map, $requiredRow, $requiredPlace);
+logReserve($requiredRow, $requiredPlace, $reverve);
+
+    function logReserve($row, $place, $result){
+        if ($result) {
+            echo "Ваше место забронировано! Ряд $row, место $place <br>".PHP_EOL;
+        } else {
+            echo "Что-то пошло не так=( Бронь не удалась <br>".PHP_EOL;
+        }
+    }
+
+?>
---
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -0,0 +1,46 @@
+<?php
+
+    function generate($rows, $placesPerRow, $chairs){
+        if ($rows * $placesPerRow > $chairs) {
+            return false;
+        }
+        $arr = [];
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $placesPerRow; $j++) {
+                $arr[$i][$j] = false;
+            }
+        }
+        return $arr;
+    }    
+
+    function reserve($map, $requiredPlaces) {
+        for ($i=0; $i<count($map); $i++) {
+
+            $value = $requiredPlaces;
+            for ($j=0; $j<count($map[$i]); $j++) {
+                if (($j + $value) > (count($map[$i]))) {
+                    return false;
+                }
+                if ($map[$i][$j] === false) {
+                    $value--;
+                    if ($value === 0) {
+                        return 'Найдены лучшие места: c '.($j-$requiredPlaces+2).' по '.($j+1).' в ряду '.($i+1);
+                    }
+                }
+                else {
+                    $value = $requiredPlaces;
+                }
+            }
+        }    
+        return 'Подходящих мест не найдено';            
+    }
+
+    $chairs = 50;
+    $map = generate(5, 8, $chairs);
+    $map[2][0] = true;
+    $map[6][3] = true;
+    $requiredPlaces = 6;
+    
+    echo reserve($map, $requiredPlaces);
+
+?>
